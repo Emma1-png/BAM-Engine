ee5f2ad895d5db9369e172e3b5c2be2f4b8bf6d5
Emma <moddo5120@gmail.com>
2025-11-03 19:23:24 +0000

    Add FrontEnd and BackEnd classes with basic functionality
    Implemented FrontEnd and BackEnd classes for the engine, including window initialization and JSON reading functionality.

---
+++ ENGINE.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+import PIL as pi
+import math as ma
+import json as jsn
+
+# Engine imports
+import audioEngine as ae         # Emits audio when needed
+import flatRenderer as fr        # Renders the shaoe of the .png given
+import lightEngine as le         # Renders light
+import textureRenderer as tr     # Renders the textures on top of flatRenderer
+
+class FrontEnd:
+    def __init__(self, windowSizeX, windowSizeY):
+        self.windowSizeX = windowSizeX
+        self.windowSizeY = windowSizeY
+    
+    def InitialiseWindow(self):
+        wn = tk.Tk()
+        wn.geometry(f"{self.windowSizeX}x{self.windowSizeY}")
+        wn.title(f"BAM! Engine - {}") # TO DO: GET THE NAME OF THE CURRENTLY LOADED PROJECT AND INPUT IT INTO THE STRING (ReadJSON?)
+        
+        wn.resizable(0, 0)
+        
+        self.wn = wn
+        
+    def WindowDisplay(self):
+        '''
+        This is for all of the buttons and various items within the frontend of the engine
+        Along with all of the borders for the sections in the app (see DevlogAndNotes.txt)
+        '''
+
+        self.c = tk.Canvas(self.wn, width=self.windowSizeX, height=self.windowSizeY)
+        self.c.pack(fill="both", expand=True)
+        
+        
+        
+        self.wn.mainloop()
+
+class BackEnd:
+    def __init__(self):
+        pass
+    
+    def Save(self):
+        #with open("projects.json", "a") as f:
+            #self.saveLocation = 
+        #with open("
+        pass
+        
+    def ReadJSON(self, option):
+        with open("projects.json", "r") as f:
+            data = jsn.load(f)
+            
+            if option == "ProjectName":
+                for project in data["projects"]:
+                    print(project["ProjectName"], "->", project["SavePath"])
+                
+fe = FrontEnd(900, 500)
+be = BackEnd()
+fe.InitialiseWindow()
+be.ReadJSON("ProjectName")
+fe.WindowDisplay()
